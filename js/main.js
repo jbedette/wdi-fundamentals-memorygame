be12f2c081d214920636dedc41d7015f021bfaea
jbedette <jbedette@yahoo.com>
2017-07-14 04:52:59 -0400

    matching working

--- js/main.js
+++ js/main.js
@@ -1,19 +1,24 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+  {rank: "queen", suit: "hearts", cardImage: "images/queen-of-hearts.png"},
+  {rank: "queen", suit: "diamonds", cardImage: "images/queen-of-diamonds.png"},
+  {rank: "king", suit: "hearts", cardImage: "images/king-of-hearts.png"},
+  {rank: "king", suit: "diamonds", cardImage: "images/king-of-diamonds.png"}
+];
 var cardsInPlay = [];
 
 function flipCard(cardId) {
 
-  console.log("User flipped " + cards[cardId]);
-  cardsInPlay.push(cardId);
+  console.log("User flipped " + cards[cardId].rank);
+  console.log(cards[cardId].suit);
+  console.log(cards[cardId].cardImage);
+  cardsInPlay.push(cards[cardId].rank);
+  console.log(cardsInPlay)
   if (cardsInPlay.length === 2){
-    if (cardId === cardId) {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
       alert("You found a match")
     }
     else {
       alert("Try again")
     }
   };
-  console.log('user flipped ' + cardsInPlay);
-
-
-}
+};
