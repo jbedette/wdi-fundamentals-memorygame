69bcb32b8ce629648bcb0cb1b3ef1d38ded28eea
jbedette <jbedette@yahoo.com>
2017-07-14 00:57:50 -0400

    added js for game

--- js/main.js
+++ js/main.js
@@ -1,9 +1,20 @@
-console.log("Up and running!");
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cardOne = cards[0];
+var cardTwo = cards[2];
+var cardThree = cards[1];
+var cardFour = cards[3];
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardThree)
+
+if (cardsInPlay.length === 2){
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    alert("You found a match")
+  }
+  else {
+    alert("Try again")
+  }
+};
+console.log('user flipped ' + cardsInPlay);
